8252fa9ab0317b9ff1880d3473c04e75378ee486
racka98 <bmkilaha@gmail.com>
2021-06-21 18:01:54 +0300

    switch to CardView

--- app/src/main/java/work/racka/wallpapergallery/BindingAdapters.kt
+++ app/src/main/java/work/racka/wallpapergallery/BindingAdapters.kt
@@ -5,6 +5,8 @@ import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import work.racka.wallpapergallery.main.WallpaperGridAdapter
 import work.racka.wallpapergallery.network.WallpaperProperty
 
@@ -14,6 +16,12 @@ fun bindImage(image: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(image.context)
             .load(imgUri)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            /**.apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+            )**/
             .into(image)
     }
 }
